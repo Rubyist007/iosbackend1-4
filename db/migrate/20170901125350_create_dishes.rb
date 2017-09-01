5e39f8410c4a085ef89de4ac2028dee2012171c1
VasiliyDybinchyc <VasiliyDybinchyk@gmail.com>
2017-09-01 16:37:27 +0300

    add associations dish belongs to menu and add index in table db, add colum in table

--- db/migrate/20170901125350_create_dishes.rb
+++ db/migrate/20170901125350_create_dishes.rb
@@ -1,7 +1,14 @@
 class CreateDishes < ActiveRecord::Migration[5.1]
   def change
     create_table :dishes do |t|
-
+      t.belongs_to :menu, index: true
+      t.string :name
+      t.string :description
+      t.string :photo
+      t.string :ingredients
+      t.integer :number_of_ratings
+      t.integer :average_rating
+      t.integer :sum_rating
       t.timestamps
     end
   end
