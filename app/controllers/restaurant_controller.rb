1132fc551a7ae729a49688c13eb78bc3ffc775fd
VasiliyDybinchyc <VasiliyDybinchyk@gmail.com>
2017-09-05 15:30:39 +0300

    add restaurant controller

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,22 @@
+class RestaurantController < ApplicationController
+
+  def index
+    render json: Restaurant.all.first(10)
+  end
+
+  def create
+    restaurant = Restaurant.new(restaurant_params)
+    restaurant.save
+    render json: restaurant
+  end
+
+  def show 
+    render json: Restaurant.find(params[:id])
+  end
+
+  private
+
+    def restaurant_params 
+      params.require(:restaurant).permit(:title , :description)
+    end
+end
