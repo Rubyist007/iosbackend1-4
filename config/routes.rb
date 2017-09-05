191def7f74d229dc07cf68863443368f55720142
VasiliyDybinchyc <VasiliyDybinchyk@gmail.com>
2017-09-05 15:28:31 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurant, only: [:create, :show, :index]
+  resources :menu, only: [:create, :show]
+  resources :dish, only: [:create, :show, :index]
+  resources :evaluation, only: [:create, :show, :index]
 end
