54367a7f8d30afdcfe47aaca363ad710759a5265
VasiliyDybinchyc <VasiliyDybinchyk@gmail.com>
2017-09-05 14:09:11 +0300

    add validate restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
   has_one :menu
+
+  validates_presence_of :title, :description#, :main_photo
+
+  validates :title, length: { maximum: 50 }
+
+  validates :description, length: { minimum: 100, maximum: 500 }
 end
