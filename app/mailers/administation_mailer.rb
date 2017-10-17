e5b743af8b372f7e1ab54dd39842f4f0ba8221ad
Ivancha <sg.igor.iv@gmail.com>
2017-10-17 14:06:27 +0300

    test

--- app/mailers/administation_mailer.rb
+++ app/mailers/administation_mailer.rb
@@ -2,7 +2,7 @@ class AdministationMailer < ApplicationMailer
   default from: "test@R8.com"
 
   def messaage
-    mail from: "test@R8.com", to: "vasargkvasargl@gmail.com", subject: "test" 
+    mail from: "test@R8.com", to: "vasargkvasargl@gmail.com", subject: "test", text: 'Done?' 
     #render json: 'Done'
   end
 end
