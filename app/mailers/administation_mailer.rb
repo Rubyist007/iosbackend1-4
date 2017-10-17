6524587e8fd412778371f632efbe584dad5932cc
Ivancha <sg.igor.iv@gmail.com>
2017-10-17 14:20:34 +0300

    test

--- app/mailers/administation_mailer.rb
+++ app/mailers/administation_mailer.rb
@@ -2,7 +2,15 @@ class AdministationMailer < ApplicationMailer
   default from: "test@R8.com"
 
   def messaage
-    mail from: "test@R8.com", to: "vasargkvasargl@gmail.com", subject: "test", text: 'Done?' 
+    mg_client = Mailgun::Client.new 'key-cf696ec28d426e53abaf0a3653ce7dea' 
+
+    message_params = {from: 'testuset@gmail.com',
+                to: 'vasargkvasargl@gmail.com',
+                subject: 'etst',
+                text: 'test'}
+
+    mg_client.seng_message 'sandbox6c8063d955234a3a8d59ebe240367df1.mailgun.org', message_params
+    #mail from: "test@R8.com", to: "vasargkvasargl@gmail.com", subject: "test", text: 'Done?' 
     #render json: 'Done'
   end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
+  require 'mailgun'
+
   default from: 'administration-R8@example.com'
   layout 'mailer'
+
+
 end
