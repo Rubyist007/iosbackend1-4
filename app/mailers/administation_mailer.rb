468a6338f0553ddf66d76caa1326533ff8e58b09
Ivancha <sg.igor.iv@gmail.com>
2017-10-17 12:14:37 +0300

    test

--- app/mailers/administation_mailer.rb
+++ app/mailers/administation_mailer.rb
@@ -1,5 +1,5 @@
 class AdministationMailer < ApplicationMailer
-  default from: "#{current_user.email}"
+  default from: "sandbox46b5b95dac97447ca3b162b57a058f6d.mailgun.org"
 
   def messaage
     mail(to: "vasargkvasargl@gmail.com", subject: "test")   
