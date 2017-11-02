5bc76ee1b362d5f84560cc8700dbd0b28d905f05
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-02 23:03:59 +0200

    test

--- app/controllers/override_device_controller/omniauth_callbacks_controller.rb
+++ app/controllers/override_device_controller/omniauth_callbacks_controller.rb
@@ -49,6 +49,7 @@ class OverrideDeviceController::OmniauthCallbacksController < DeviseTokenAuth::O
 
     #new_auth_header = @resource.create_new_auth_token
     response.headers.merge!(auth_header)
+    #response.headers.merge!({'Location' => 'google.com'})
 
     #render json: @resource
     redirect_to 'r8ProdUrl://'
