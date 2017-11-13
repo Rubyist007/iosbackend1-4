8ebc383ecfdd7747676c96d08be4754633f66bef
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-13 11:26:11 +0200

    change SendGrid username and password

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: 'app80912331@heroku.com',
-  password: 'xz9fyr4b9808',
+  user_name: 'app80912783@heroku.com',
+  password: 'lb6kvdjl6743',
   domain: 'https://iosbackend1-4.herokuapp.com/',
   address: 'smtp.sendgrid.net',
   port: '587',
