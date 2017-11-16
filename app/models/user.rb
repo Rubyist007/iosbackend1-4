2d0805c75c5eba8f4a59b05c0da29b98cbe9d271
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-16 12:31:00 +0200

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,11 +74,10 @@ class User < ActiveRecord::Base
 
       near_restaurant.each do |r|
         p '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-
         p r.dishes.count
         p offset = rand(r.dishes.count)
         p dishes << r.dishes.offset(offset).first
-        p '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+        p '**************************************************'
       end
 
       dishes.each do |d|
