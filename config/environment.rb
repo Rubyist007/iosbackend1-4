863b56d9dc027feb220466dff09a41252de9088c
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-20 15:38:56 +0200

    new key sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: 'app80912783@heroku.com',
-  password: 'lb6kvdjl6743',
+  user_name: 'app81371379@heroku.com',
+  password: 'f7j8if682892',
   domain: 'https://iosbackend1-4.herokuapp.com/',
   address: 'smtp.sendgrid.net',
   port: '587',
