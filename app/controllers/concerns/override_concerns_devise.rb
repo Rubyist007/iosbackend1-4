da27a4673fd115bc7081a099617c976d44bca182
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-24 16:04:51 +0200

    new password reset

--- app/controllers/concerns/override_concerns_devise.rb
+++ app/controllers/concerns/override_concerns_devise.rb
@@ -1,8 +1,7 @@
 module Devise::Models::Recoverable
 
   def set_reset_password_token
-    raw = [1,1,1,1,1,1]
-    raw.map! { rand(10) }
+    raw = (0..5).map { rand(10) }.join
     enc = Devise.token_generator.digest(User, :reset_password_token, raw)
 
     self.reset_password_token = enc
