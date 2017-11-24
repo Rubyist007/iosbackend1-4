3d1bb7a9583cea6849d71818821e558aca106746
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-24 11:43:37 +0200

    render update user in array

--- app/controllers/override_device_controller/registrations_controller.rb
+++ app/controllers/override_device_controller/registrations_controller.rb
@@ -4,10 +4,17 @@ class OverrideDeviceController::RegistrationsController < DeviseTokenAuth::Regis
     render json: {data: [@resource]}
   end
 
+  def render_update_success
+    render json: {data: [@resource]}
+  end
+
   def render_create_error
     render json: { status: 422, errors: resource_errors[:full_messages] }, status: 422
   end
-
+  
+  def render_update_error
+    render json: { status: 422, errors: resource_errors[:full_messages] }, status: 422
+  end
   #def render_create_success
   #  render json: { status: 201, Data: @resource}
   #end
