6d094476733972ab7b09add637ed801068e38374
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-24 13:38:36 +0200

    new password reset

---
+++ app/controllers/concerns/override_concerns_devise.rb
@@ -0,0 +1,12 @@
+module Devise::Concerns::User
+
+  def set_reset_password_token
+raw, enc = '1234567' #Devise.token_generator.generate(self.class, :reset_password_token)
+
+    self.reset_password_token = enc
+    self.reset_password_sent_at = Time.now.utc
+    save(validate: false)
+    raw
+  end
+
+end
