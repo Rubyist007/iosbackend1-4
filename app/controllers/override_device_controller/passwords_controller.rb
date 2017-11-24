951fe3576bbc62f932cf6670cf2ffcbf3bf54adc
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-24 16:17:21 +0200

    new password reset

--- app/controllers/override_device_controller/passwords_controller.rb
+++ app/controllers/override_device_controller/passwords_controller.rb
@@ -3,10 +3,7 @@ class OverrideDeviceController::PasswordsController < DeviseTokenAuth::Passwords
   skip_after_action :update_auth_header, :only => [:create, :edit]
 
   def update
-    # make sure user is authorized
-    unless @resource
-      return render_update_error_unauthorized
-    end
+    @resource = User.where(uid: params[:email])
 
     # make sure account doesn't use oauth2 provider
     unless @resource.provider == 'email'
@@ -17,20 +14,14 @@ class OverrideDeviceController::PasswordsController < DeviseTokenAuth::Passwords
 
     token = Devise.token_generator.digest(User, :reset_password_token, params[:reset_password_token])
 
-    p "!!!!!!!!!!!!!!!!!!!!!!!!"
-    p @resource.reset_password_token
-    p token
-    p "!!!!!!!!!!!!!!!!!!!!!!!!"
-
     unless token == @resource.reset_password_token
       return render json: { errors: "wrong reset_password_token" }
     end
 
     # ensure that password params were sent
-    
-    #unless params[:password] && params[:password_confirmation]
-    #  return render_update_error_missing_password
-    #end
+    unless params[:password] && params[:password_confirmation]
+      return render_update_error_missing_password
+    end
 
     if @resource.update_attributes(password: params[:password])
       @resource.allow_password_change = true
