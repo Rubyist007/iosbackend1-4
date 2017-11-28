ce8cf1736d2879084c347ee44602f304c3776b79
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-28 19:11:45 +0200

    add api keys to yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
+api_keys = YAML::load_file("#{Rails.root}/config/api_keys.yml")[Rails.env]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :instagram, '1e595466ab8a4671a4210d974b5008bf', '14eed34d8a9244338c6fb85d308e0be5'
-  provider :google_oauth2, '505150086257-jmhnraghosdeaq8aum31bhhfqb2qmae0.apps.googleusercontent.com', 'TvzlFh2x3EaLfETTpnZ4Pl95', skip_jwt: true
-  provider :twitter, '7AnydeAscaWXICHZ5qUkq35Vf', 'GqPPSNqCgZXQuBf962XKaaijawifMlz7X7bWDI6OWSZFax8Z9k'
-  provider :facebook, '142528229808784', 'a85b8e4107bd9f083a754d08feb18fee'
+  provider :instagram, api_keys['google_oAuth']['api_key'], api_keys['google_oAuth']['api_secret']
+  provider :google_oauth2, api_keys['google_oAuth']['api_key'], api_keys['google_oAuth']['api_secret'], skip_jwt: true
+  provider :twitter, api_keys['google_oAuth']['api_key'], api_keys['google_oAuth']['api_secret']
+  provider :facebook, api_keys['facebook_oAuth']['api_key'], api_keys['facebook_oAuth']['api_secret']
 end
 
 
