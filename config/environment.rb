f64a1c3752a8f38f7df1d388bead47079096ff8b
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-11-29 16:24:17 +0200

    fix password sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: 'app81888423@heroku.com',
-  password: 'd0e7t9d8227',
+  password: 'd0e7t9d82275',
   domain: 'https://iosbackend1-4.herokuapp.com/',
   address: 'smtp.sendgrid.net',
   port: '587',
