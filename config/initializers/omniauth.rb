5f59635a3021ca8d23362ecb77819cd74f485e3f
Rubyist007 <vasargkvasargl@gmail.com>
2017-12-03 21:53:48 +0000

    Api keys safe now, ignore public/uploads and secrets decryption key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,8 @@
-api_keys = YAML::load_file("#{Rails.root}/config/api_keys.yml")[Rails.env]
+api_keys = Rails.application.secrets
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :instagram, api_keys['google_oAuth']['api_key'], api_keys['google_oAuth']['api_secret']
-  provider :google_oauth2, api_keys['google_oAuth']['api_key'], api_keys['google_oAuth']['api_secret'], skip_jwt: true
-  provider :twitter, api_keys['google_oAuth']['api_key'], api_keys['google_oAuth']['api_secret']
-  provider :facebook, api_keys['facebook_oAuth']['api_key'], api_keys['facebook_oAuth']['api_secret']
+  provider :google_oauth2, api_keys[:google_oAuth][:api_key], api_keys[:google_oAuth][:api_secret], skip_jwt: true
+  provider :instagram, api_keys[:instagram_oAuth][:api_key], api_keys[:instagram_oAuth][:api_secret] 
+  provider :twitter, api_keys[:twitter_oAuth][:api_key], api_keys[:google_oAuth][:api_secret]
+  provider :facebook, api_keys[:facebook_oAuth][:api_key], api_keys[:facebook_oAuth][:api_secret]
 end
-
-
